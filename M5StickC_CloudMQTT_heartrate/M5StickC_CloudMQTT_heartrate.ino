4e14eefebe635c889815f339a426eafc449ab2f0
Hasebe Masahiko <JCA03205@gmail.com>
2020-11-06 20:02:38 +0900

    Create M5StickC_CloudMQTT_heartrate.ino

---
+++ M5StickC_CloudMQTT_heartrate/M5StickC_CloudMQTT_heartrate.ino
@@ -0,0 +1,211 @@
+
+//
+//  Hamamatsu Project
+//    Data Transfer System by MQTT for M5StickC
+//
+//    Nov.6 , 2020 FabLab Hamamatsu
+//
+/*
+    pls install MAX30100lib by library manager first
+    addr: https://github.com/oxullo/Arduino-MAX30100
+*/
+
+#include <M5StickC.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <string>
+#include "MAX30100.h"
+
+
+
+// WiFi settings
+const char ssid[] = "";         //  #### Your Wifi ID
+const char password[] = "";     //  #### Your Wifi PW
+
+WiFiClient wifiClient;
+
+// MQTT settings
+const char* mqttBrokerAddr = "";
+const char* mqttUserName = "";
+const char* mqttPassword = "";
+const int mqttPort = 0;
+const char* mqttClientID = "BNKK_HEARTRATE";   // unique id that anything you like
+PubSubClient mqttClient(wifiClient);
+const String yourDevice("BNKK_heartrate");        //  #### Your Device
+
+const String swTopic("M5SW");
+
+unsigned long lastUpdateTime = 0;
+unsigned long lastUpdateTime10ms = 0;
+
+// Sampling is tightly related to the dynamic range of the ADC.
+// refer to the datasheet for further info
+#define SAMPLING_RATE                       MAX30100_SAMPRATE_100HZ
+
+// The LEDs currents must be set to a level that avoids clipping and maximises the
+// dynamic range
+#define IR_LED_CURRENT                      MAX30100_LED_CURR_50MA
+#define RED_LED_CURRENT                     MAX30100_LED_CURR_27_1MA
+
+// The pulse width of the LEDs driving determines the resolution of
+// the ADC (which is a Sigma-Delta).
+// set HIGHRES_MODE to true only when setting PULSE_WIDTH to MAX30100_SPC_PW_1600US_16BITS
+#define PULSE_WIDTH                         MAX30100_SPC_PW_1600US_16BITS
+#define HIGHRES_MODE                        true
+
+// Instantiate a MAX30100 sensor class
+MAX30100 sensor;
+
+void setup() {
+
+  int wifiCheckCount = 0;
+  int wifiExitCount = 0;
+
+  M5.begin();
+  initPrintSomewhere();
+  WiFi.begin(ssid, password);
+  Serial.begin(115200);
+
+  // attempt to connect to Wifi network:
+  while (WiFi.status() != WL_CONNECTED) {
+    printSomewhere(".");
+    delay(500);
+    if ( ++wifiCheckCount > 10 ){ // 5sec
+      printSomewhere("\n");
+      printSomewhere("Retry Wifi Connection.\n");
+      WiFi.begin(ssid, password);
+      wifiCheckCount = 0;
+      if ( ++wifiExitCount > 10 ){
+        printSomewhere("\n");
+        printSomewhere("No Wifi Connection!\n");
+        break;
+      }
+    }
+  }
+
+  //  MQTT
+  mqttClient.setServer(mqttBrokerAddr, mqttPort);
+  mqttClient.setCallback(mqttCallback);
+  printSomewhere("\n");
+
+  // Initialize the sensor
+  // Failures are generally due to an improper I2C wiring, missing power supply
+  // or wrong target chip
+  if (!sensor.begin()) {
+    Serial.println("FAILED");
+    for(;;);
+  } else {
+    Serial.println("SUCCESS");
+  }
+
+  // Set up the wanted parameters
+  sensor.setMode(MAX30100_MODE_SPO2_HR);
+  sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT);
+  sensor.setLedsPulseWidth(PULSE_WIDTH);
+  sensor.setSamplingRate(SAMPLING_RATE);
+  sensor.setHighresModeEnabled(HIGHRES_MODE);
+}
+
+void loop() {
+  unsigned long t = millis();
+  M5.update();
+  reConnect();
+  mqttClient.loop();
+
+  // update a value from sensor every 10ms
+  if (t - lastUpdateTime10ms >= 10) {
+    lastUpdateTime10ms = t;
+    //  something every 10msec
+  }
+
+  // update a value from sensor every 1000ms
+  if (t - lastUpdateTime >= 1000) {
+    lastUpdateTime = t;
+    //  something every 1000msec
+
+  }
+
+  uint16_t ir, red;
+  sensor.update();
+
+  while (sensor.getRawValues(&ir, &red)) {
+    const String topicStr = yourDevice;
+    const char* const topic = topicStr.c_str();
+    const String msgStr = String(ir)+"-"+String(red);
+    const char* msg = msgStr.c_str();
+    printSomewhere(topic);
+    printSomewhere(msg);
+    mqttClient.publish(topic, msg);
+  }
+
+  // Switch Enable/Disable MQTT transfer by pushing button B
+  if (M5.BtnB.wasPressed()) {
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(0, 0);
+  }
+  // Push Button A
+  if (M5.BtnA.wasPressed()) {
+    // Send
+    const String topicStr = yourDevice+"/"+swTopic+"/A";
+    const char* const topic = topicStr.c_str();
+    const char* const msg = "Pressed";
+    printSomewhere(topic);
+    printSomewhere(msg);
+    mqttClient.publish(topic, msg);
+  }
+  // Release Button A
+  if (M5.BtnA.wasReleased()) {
+    // Send
+    const String topicStr = yourDevice+"/"+swTopic+"/A";
+    const char* const topic = topicStr.c_str();
+    const char* const msg = "Released";
+    printSomewhere(topic);
+    printSomewhere(msg);
+    mqttClient.publish(topic, msg);   
+  }
+}
+
+//-------------------------------
+//  MQTT
+//-------------------------------
+void mqttCallback(char* topic, byte* payload, unsigned int length)
+{
+}
+
+void reConnect() { // 接続が切れた際に再接続する
+  static unsigned long lastFailedTime = 0;
+  static boolean lastConnect = false;
+  unsigned long t = millis();
+  if (!mqttClient.connected()) {
+    if (lastConnect || t - lastFailedTime >= 5000) {
+      if (mqttClient.connect(mqttClientID, mqttUserName, mqttPassword)) {
+        printSomewhere("MQTT Connect OK.");
+        lastConnect = true;
+        mqttClient.subscribe("#");
+      } else {
+        printSomewhere("MQTT Connect failed, rc=");
+        // http://pubsubclient.knolleary.net/api.html#state に state 一覧が書いてある
+        printSomewhere(mqttClient.state());
+        lastConnect = false;
+        lastFailedTime = t;
+      }
+    }
+  }
+}
+
+//-------------------------------
+//  Print for Debug
+//-------------------------------
+void initPrintSomewhere(void)
+{
+}
+void printSomewhere(int num)
+{
+  char strx[128]={0};
+  sprintf(strx,"%d",num);
+  M5.Lcd.printf("%s",strx);
+}
+void printSomewhere(const char* txt)
+{
+  M5.Lcd.printf(txt);
+}
